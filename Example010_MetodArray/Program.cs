4a399b5549d53430da127e586fcf8fb85d78920b
AleksandraMN <al_mn@mail.ru>
2022-08-07 18:24:13 +1000

    MetodArray

---
+++ Example010_MetodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array = {1, 3, 8, 4, 5, 7, 9, 5};
+int n = array.Length;
+int find = 5;
+
+int index = 0;
+while(index< n)
+{
+	if (array[index] == find)
+	{
+		Console.WriteLine(index);
+		break;
+	}
+	index++;
+}
